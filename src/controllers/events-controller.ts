38f0722047cd16e8c6444d66210a8e25f8d19681
Levy <levymcruz@gmail.com>
2023-10-20 13:55:26 -0300

    Adding reddis into Event route

--- src/controllers/events-controller.ts
+++ src/controllers/events-controller.ts
@@ -1,8 +1,20 @@
 import { Request, Response } from 'express';
 import httpStatus from 'http-status';
 import { eventsService } from '@/services';
+import { Redis } from 'ioredis';
+
+const redis = new Redis(
+  'rediss://red-ckoqa38ujous73dsvs80:JT6f4Ch7OAEudIDwkcouOcAcwM7OBpNv@oregon-redis.render.com:6379',
+);
 
 export async function getDefaultEvent(_req: Request, res: Response) {
+  const redisRes = await redis.get('event');
+  if (redisRes) {
+    console.info('Foi pelo Redis !!!');
+    return res.status(httpStatus.OK).send(JSON.parse(redisRes));
+  }
   const event = await eventsService.getFirstEvent();
+  await redis.set('event', JSON.stringify(event));
+  console.info('Foi banco normal');
   return res.status(httpStatus.OK).send(event);
 }
--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,8 @@ import app, { init } from '@/app';
 
 const port = +process.env.PORT || 4000;
 
+// redis://red-ckoqa38ujous73dsvs80:6379
+
 init().then(() => {
   app.listen(port, () => {
     /* eslint-disable-next-line no-console */
