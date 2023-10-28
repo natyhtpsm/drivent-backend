657c041a8254d7a5e2ec5ec5b307e5dfbec756bc
natyhtpsm <119613135+natyhtpsm@users.noreply.github.com>
2023-10-28 17:43:16 -0300

    Update: seed.ts

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -24,60 +24,79 @@ async function main() {
       description: "Descrição da Atividade 1",
       schedule: dayjs().add(1, "day").toDate(),
       capacity: 100,
+      duration: 1,
     },
     {
       name: "Atividade 2",
       description: "Descrição da Atividade 2",
       schedule: dayjs().add(2, "days").toDate(),
       capacity: 150,
+      duration: 2,
+
     },
     {
       name: "Atividade 3",
       description: "Descrição da Atividade 3",
       schedule: dayjs().add(3, "days").toDate(),
       capacity: 80,
+      duration: 1,
+
     },
     {
       name: "Atividade 4",
       description: "Descrição da Atividade 4",
       schedule: dayjs().add(4, "days").toDate(),
       capacity: 120,
+      duration: 3,
+
     },
     {
       name: "Atividade 5",
       description: "Descrição da Atividade 5",
       schedule: dayjs().add(5, "days").toDate(),
       capacity: 90,
+      duration: 1,
+
     },
     {
       name: "Atividade 6",
       description: "Descrição da Atividade 6",
       schedule: dayjs().add(6, "days").toDate(),
       capacity: 110,
+      duration: 1,
+
     },
     {
       name: "Atividade 7",
       description: "Descrição da Atividade 7",
       schedule: dayjs().add(7, "days").toDate(),
       capacity: 130,
+      duration: 2,
+
     },
     {
       name: "Atividade 8",
       description: "Descrição da Atividade 8",
       schedule: dayjs().add(8, "days").toDate(),
       capacity: 70,
+      duration: 2,
+
     },
     {
       name: "Atividade 9",
       description: "Descrição da Atividade 9",
       schedule: dayjs().add(9, "days").toDate(),
       capacity: 160,
+      duration: 1,
+
     },
     {
       name: "Atividade 10",
       description: "Descrição da Atividade 10",
       schedule: dayjs().add(10, "days").toDate(),
       capacity: 140,
+      duration: 1,
+
     },
   ];  
 
