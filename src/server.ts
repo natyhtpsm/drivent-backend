00249342eb44c97ebda4420773aec01f11fa881a
GabrielIGNS <gabrielgranjeia@alunos.utfpr.edu.br>
2023-10-29 19:52:11 -0300

    fix/minor correction

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import app, { init } from '@/app';
+import RedisClient from './config/redisConfig';
 
 const port = +process.env.PORT || 4000;
 
 init().then(() => {
   app.listen(port, () => {
     /* eslint-disable-next-line no-console */
+    RedisClient.connect();
     console.log(`Server is listening on port ${port}.`);
   });
 });
